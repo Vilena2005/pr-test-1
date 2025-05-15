82c9e1d31e2c5c1ba2f7266fd848b7ceac061447
Вилена <Vilena5002@mail.ru>
2025-05-15 16:47:27 +0700

    Add 2 negative scenario

---
+++ cypress/e2e/Negative/NegativeEmployerLogin.cy.js
@@ -0,0 +1,25 @@
+describe('Авторизация работодателя', () => {
+    it('Успешный вход',() => {
+        cy.fixture('EmployerLog').then(data => {
+            cy.log('Переход на страницу авторизации')
+            cy.visit("https://dev.profteam.su/login")
+
+            cy.log('Ввод неправильного логина работодателя')
+            cy.get('.form-input--text')
+                .type(data.nonCorrectLogin)
+
+            cy.log('Ввод неправильного пароля работодателя')
+            cy.get('.form-input--password')
+                .type(data.nonCorrectPassword)
+
+            cy.log('Клик по кнопке "Войти"')
+            cy.get(':nth-child(3) > .button')
+                .click()
+            cy.wait(3000);
+
+            cy.log('Проверка что появился элемент сигнализирующий об ошибке')
+            cy.get('.form-error > span')
+                .should('exist')
+        })
+    })
+})
---
+++ cypress/e2e/Positive/studentResponse.cy.js
@@ -0,0 +1,23 @@
+describe('Отлик на вакансию студентом', () => {
+    it('Успешный отлик',() => {
+        cy.fixture('studentLog').then(data => {
+            cy.log('Переход на страницу авторизации')
+            cy.visit("https://dev.profteam.su/login")
+
+            cy.log('Ввод логина студента')
+            cy.get('.form-input--text')
+                .type(data.login)
+
+            cy.log('Ввод пароля студента')
+            cy.get('.form-input--password')
+                .type(data.password)
+
+            cy.log('Клик по кнопке "Войти"')
+            cy.get(':nth-child(3) > .button')
+                .click()
+            cy.wait(3000);
+
+
+        })
+    })
+})
